a60efc99a00c29373563d83fef94ac9e0ab1acff
Nick Lamarca <nicklamarca64@gmail.com>
2021-12-14 15:21:56 -0600

    Add Razor Page SodaMachine Logic

---
+++ SodaMachineRazorUI/Pages/SodaMachine.cshtml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SodaMachineLibrary.Logic;
+using SodaMachineLibrary.Models;
+
+namespace SodaMachineRazorUI
+{
+    public class SodaMachineModel : PageModel
+    {
+        private readonly ISodaMachineLogic _sodaMachine;
+
+        public decimal SodaPrice { get; set; }
+        public decimal DepositedAmount { get; set; }
+        public List<SodaModel> SodaOptions { get; set; }
+
+        [BindProperty]
+        public decimal Deposit { get; set; }
+
+        [BindProperty]
+        public SodaModel SelectedSoda { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string UserId { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string OutputText { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ErrorMessage { get; set; }
+
+        public SodaMachineModel(ISodaMachineLogic sodaMachine)
+        {
+            _sodaMachine = sodaMachine;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(UserId))
+            {
+                UserId = Guid.NewGuid().ToString();
+            }
+
+            SodaPrice = _sodaMachine.GetSodaPrice();
+            DepositedAmount = _sodaMachine.GetMoneyInsertedTotal(UserId);
+            SodaOptions = _sodaMachine.ListTypesOfSoda();
+        }
+
+        // Used for depositing coins
+        public IActionResult OnPost()
+        {
+            if (Deposit > 0)
+            {
+                _sodaMachine.MoneyInserted(UserId, Deposit);
+            }
+
+            return RedirectToPage(new { UserId });
+        }
+
+        // Used for requesting soda
+        public IActionResult OnPostSoda()
+        {
+            var results = _sodaMachine.RequestSoda(SelectedSoda, UserId);
+            OutputText = "";
+
+            // Handle the response
+            if (results.errorMessage.Length > 0)
+            {
+                ErrorMessage = results.errorMessage;
+            }
+            else
+            {
+                OutputText = $"Here is your { results.soda.Name }";
+
+                if (results.change.Count > 0)
+                {
+                    OutputText += "<br><br>Here is your change:<br>";
+                    results.change.ForEach(x => OutputText += $"{ x.Name }<br>");
+                }
+                else
+                {
+                    OutputText += "<br><br>You used exact change so there is no change to refund.";
+                }
+            }
+
+            return RedirectToPage(new { UserId, ErrorMessage, OutputText });
+        }
+
+        // Used for cancelling our deposit
+        public IActionResult OnPostCancel()
+        {
+            DepositedAmount = _sodaMachine.GetMoneyInsertedTotal(UserId);
+            _sodaMachine.IssueFullRefund(UserId);
+
+            OutputText = $"You have been refunded { String.Format("{0:C}", DepositedAmount) }";
+
+            return RedirectToPage(new { UserId, OutputText });
+        }
+    }
+}
--- SodaMachineRazorUI/Startup.cs
+++ SodaMachineRazorUI/Startup.cs
@@ -1,17 +1,19 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using SodaMachineRazorUI.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using SodaMachineRazorUI.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SodaMachineLibrary.DataAccess;
+using SodaMachineLibrary.Logic;
 
 namespace SodaMachineRazorUI
 {
@@ -32,7 +34,10 @@ namespace SodaMachineRazorUI
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+
+            services.AddTransient<IDataAccess, TextFileDataAccess>();
+            services.AddTransient<ISodaMachineLogic, SodaMachineLogic>();
+
             services.AddRazorPages();
         }
 
@@ -46,10 +51,11 @@ namespace SodaMachineRazorUI
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -60,9 +66,6 @@ namespace SodaMachineRazorUI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
